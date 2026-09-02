7290ae7cceb3c83c57a0cade88e77ee6d0e44d0e
Dan Truong <DanTruong@users.noreply.github.com>
2026-09-02 00:20:54 -0400

    Start Python fetch script

---
+++ fetch_data.py
@@ -0,0 +1,13 @@
+## Fetch data from NYS Health API
+
+## Make sure to ingest Season, Region, County, CDC Week, Week Ending, Disease, Count, County Centroid, and FIPS
+
+## Work with County, Week Ending Date (as Date), Disease, Incidents (as Count), and Coordinates (as County Centroid)
+
+## Separate Coordinates into Latitude and Longitude
+
+## Separate date into Month, Day, and Year
+
+## Rename Influenza labels as A, B, and Unspecified
+
+## Aggregate the flu data by sum of incidents (as a consolidated set)
